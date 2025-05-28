bfbe10cb8bb1b6f00e0584a93674cf983efa5e35
TRILLIONAIRE <trillionaire@MacBookPro.lan>
2025-05-28 18:49:17 -0400

    Fixed SVG encoding in whitepaper carousel

--- src/components/sections/WhitepaperCarousel.tsx
+++ src/components/sections/WhitepaperCarousel.tsx
@@ -258,7 +258,9 @@ const WhitepaperCarousel: React.FC = () => {
 
                     {/* Digital Grid Pattern */}
                     <div className="absolute inset-0 opacity-5 pointer-events-none">
-                      <div className="w-full h-full bg-[url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fill-rule="evenodd"%3E%3Cg fill="%23ffffff" fill-opacity="0.1"%3E%3Ccircle cx="30" cy="30" r="1"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')]"></div>
+                      <div className="w-full h-full" style={{
+                        backgroundImage: `url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23ffffff' fill-opacity='0.1'%3E%3Ccircle cx='30' cy='30' r='1'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`
+                      }}></div>
                     </div>
                   </motion.div>
                 </div>
